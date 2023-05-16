b93d0d63824da682b2c5dcc7bd3a86a866f4b07f
zamparg <zamparg@hotmail.com>
2023-05-15 21:31:14 -0300

    actualización 21.30

--- js/main.js
+++ js/main.js
@@ -27,10 +27,10 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
 
     // Facts counter
